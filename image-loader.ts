942030d3d082384c5d45e5a4d41449e877c67950
han19950212 <1245595765@qq.com>
2023-11-30 18:04:25 +0800

    delete all pages

--- image-loader.ts
+++
@@ -1,3 +0,0 @@
-export default function customLoader({ src }: { src: string }) {
-  return src;
-}
--- src/components/custom/AuthPagesWrapper/AuthPagesWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import "./AuthPagesWrapper.scss";
-
-const AuthPagesWrapper = (props: any) => {
-  return <div className="AuthPagesWrapper">{props.children}</div>;
-};
-
-export default AuthPagesWrapper;
